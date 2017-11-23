ed8a46460a5223577c2d2d97b665c8f5f27ca3bd
Administrator <dj5827143@163.com>
2017-11-23 23:05:31 +0800

    微服务消费端

--- microcloud-provider-dept-8001/src/main/java/cn/mldn/microcloud/provider/dept/rest/DeptRest.java
+++ microcloud-provider-dept-8001/src/main/java/cn/mldn/microcloud/provider/dept/rest/DeptRest.java
@@ -3,6 +3,9 @@ package cn.mldn.microcloud.provider.dept.rest;
 import javax.annotation.Resource;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import cn.mldn.microcloud.Service.IDeptService;
@@ -12,12 +15,12 @@ import cn.mldn.microcloud.vo.Dept;
 public class DeptRest {
 	@Resource
 	private IDeptService deptService ; 
-	@GetMapping("/dept/add")
-	public Object add(Dept dept) {
+	@PostMapping("/dept/add")
+	public Object add(@RequestBody Dept dept) {
 		return this.deptService.add(dept) ; 
 	}
-	@GetMapping("/dept/get")
-	public Object get(long deptno) {
+	@GetMapping("/dept/get/{deptno}")
+	public Object get(@PathVariable("deptno") long deptno) {
 		return this.deptService.get(deptno) ; 
 	}
 	@GetMapping("/dept/list")
