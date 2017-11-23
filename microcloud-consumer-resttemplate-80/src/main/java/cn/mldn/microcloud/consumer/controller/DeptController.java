3530b696770ea7cce827ce0ce27e79a12753ca07
Administrator <dj5827143@163.com>
2017-11-23 23:59:54 +0800

    服务消费端处理

--- microcloud-consumer-resttemplate-80/src/main/java/cn/mldn/microcloud/consumer/controller/DeptController.java
+++ microcloud-consumer-resttemplate-80/src/main/java/cn/mldn/microcloud/consumer/controller/DeptController.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -18,18 +21,21 @@ public class DeptController {
 	public static final String DEPT_LIST_URL = "http://dept-8001.com:8001/dept/list" ; 
 	@Resource
 	private RestTemplate restTemplate ; 
+	@Resource
+	private HttpHeaders headers ; //有认证信息
 	@GetMapping("/consumer/dept/list")
 	public Object listDeptRest() {
-		return this.restTemplate.getForObject(DEPT_LIST_URL, List.class) ; 
+		List<Dept> allDepts = this.restTemplate.exchange(DEPT_LIST_URL, HttpMethod.GET,new HttpEntity<Object>(this.headers),List.class).getBody() ; 
+		return allDepts ; 
 	}
 	@GetMapping("/consumer/dept/get")
 	public Object getDeptRest(long deptno) {
-		Dept dept = this.restTemplate.getForObject(DEPT_GET_URL + "/" + deptno,Dept.class) ; 
+		Dept dept = this.restTemplate.exchange(DEPT_GET_URL + "/" + deptno, HttpMethod.GET, new HttpEntity<Object>(this.headers),Dept.class).getBody() ; 
 		return dept ; 
 	}
 	@GetMapping("/consumer/dept/add")
 	public boolean addDeptRest(Dept dept) {
-		Boolean flag = this.restTemplate.postForObject(DEPT_ADD_URL, dept, Boolean.class) ; 
+		Boolean flag = this.restTemplate.exchange(DEPT_ADD_URL, HttpMethod.POST, new HttpEntity<Object>(dept,this.headers),Boolean.class).getBody() ; 
 		return flag ; 
 	}
 }
